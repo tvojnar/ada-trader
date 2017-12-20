ba554f23f3188c71b6a58a9022db1a0d608455b8
Tamira Vojnar <tamiravojnar@gmail.com>
2017-12-19 16:13:24 -0800

    dynamically says if we have it or not

--- src/models/order.js
+++ src/models/order.js
@@ -6,7 +6,7 @@ const Order = Backbone.Model.extend({
 
     if (!attributes.targetPrice) {
       errors.targetPrice = ['cannot be blank'];
-    } 
+    }
 
     if (!attributes.symbol) {
       errors.symbol = ['cannot be blank'];
@@ -25,27 +25,3 @@ const Order = Backbone.Model.extend({
 }) // Order
 
 export default Order
-
-
-// validate(attributes){
-//     const errors = {}
-//       if (!attributes.title) {
-//         errors.title = ['cannot be blank'];
-//       }
-//
-//       if (!attributes.author) {
-//         errors.author = ['cannot be blank'];
-//       }
-//
-//       if (!attributes.publication_year) {
-//         errors.publication_year = ['cannot be blank'];
-//       } else if (attributes.publication_year < 1000 || attributes.publication_year > (new Date()).getFullYear() ) {
-//         errors.publication_year = ['must be between 100 and the current year']
-//       }
-//
-//       if (Object.keys(errors).length < 1) {
-//         return false
-//       }
-//
-//       return errors;
-//     }
